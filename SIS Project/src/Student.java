4b749f0ec6350797885a3cec1ba3c179410dffa6
Jack Paul <jackrpaul13@gmail.com>
2017-12-11 14:43:34 -0700

    added Student.txt and Student POJO

---
+++ SIS Project/src/Student.java
@@ -0,0 +1,80 @@
+
+public class Student
+	{
+	private String firstName, lastName, firstPeriod, firstPeriodGrade, secondPeriod, secondPeriodGrade, thirdPeriod, thirdPeriodGrade;
+		public Student(String fn, String ln, String fp, String fpg, String sp, String spg, String tp, String tpg)
+		{
+		firstName = fn;
+		lastName = ln;
+		firstPeriod = fp;
+		firstPeriodGrade = fpg;
+		secondPeriod = sp;
+		secondPeriodGrade = spg;
+		thirdPeriod = tp;
+		thirdPeriodGrade = tpg;
+		}
+		public String getFirstName()
+			{
+					return firstName;
+			}
+		public void setFirstName(String firstName)
+			{
+					this.firstName = firstName;
+			}
+		public String getLastName()
+			{
+					return lastName;
+			}
+		public void setLastName(String lastName)
+			{
+					this.lastName = lastName;
+			}
+		public String getFirstPeriod()
+			{
+					return firstPeriod;
+			}
+		public void setFirstPeriod(String firstPeriod)
+			{
+					this.firstPeriod = firstPeriod;
+			}
+		public String getFirstPeriodGrade()
+			{
+					return firstPeriodGrade;
+			}
+		public void setFirstPeriodGrade(String firstPeriodGrade)
+			{
+					this.firstPeriodGrade = firstPeriodGrade;
+			}
+		public String getSecondPeriod()
+			{
+					return secondPeriod;
+			}
+		public void setSecondPeriod(String secondPeriod)
+			{
+					this.secondPeriod = secondPeriod;
+			}
+		public String getSecondPeriodGrade()
+			{
+					return secondPeriodGrade;
+			}
+		public void setSecondPeriodGrade(String secondPeriodGrade)
+			{
+					this.secondPeriodGrade = secondPeriodGrade;
+			}
+		public String getThirdPeriod()
+			{
+					return thirdPeriod;
+			}
+		public void setThirdPeriod(String thirdPeriod)
+			{
+					this.thirdPeriod = thirdPeriod;
+			}
+		public String getThirdPeriodGrade()
+			{
+					return thirdPeriodGrade;
+			}
+		public void setThirdPeriodGrade(String thirdPeriodGrade)
+			{
+					this.thirdPeriodGrade = thirdPeriodGrade;
+			}
+	}
--- SIS Project/src/mainMenu.java
+++ SIS Project/src/mainMenu.java
@@ -4,9 +4,13 @@ public class mainMenu
 
 		public static void main(String[] args)
 			{
-				
-				System.out.println("hello");
+			addStudents();
+			}
 
+		private static void addStudents()
+			{
+			
+				
 			}
 
 	}
