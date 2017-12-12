84afb80932badb16993ce80355c687c5ec9db905
willy <willy@10.0.0.23>
2017-12-11 19:10:30 -0700

    Added an outline to follow and start of code.

--- SIS Project/src/subMenus.java
+++ SIS Project/src/subMenus.java
@@ -1,11 +1,24 @@
+import java.util.*;
 
 public class subMenus
 	{
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+				System.out.println("1) Add a Student");
+				System.out.println("2) Delete a Student");
+				
+				Scanner selection = new Scanner(System.in);
+				int pick  = selection.nextInt();
+				if(pick == 1)
+				{
+				//all implimented prior arrayList.add(firstName, lastName,firstPeriod,firstPeriodGrade, secondPeriod, secondPeriodGrade, thirdPeriod, thirdPeriodGrade)
+				System.out.println(pick);
+				}
+				else if(pick == 2)
+				{
+			    System.out.println(pick);
+				}
 			}
 
 	}
