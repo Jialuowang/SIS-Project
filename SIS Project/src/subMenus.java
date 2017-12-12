6897b69c2f47a2187972d8d14b47e94b1981fc65
willy <willy@10.0.0.23>
2017-12-11 19:10:57 -0700

    Added outline and start of code

--- SIS Project/src/subMenus.java
+++ SIS Project/src/subMenus.java
@@ -12,12 +12,14 @@ public class subMenus
 				int pick  = selection.nextInt();
 				if(pick == 1)
 				{
+				// add first name, last name, etc...
 				//all implimented prior arrayList.add(firstName, lastName,firstPeriod,firstPeriodGrade, secondPeriod, secondPeriodGrade, thirdPeriod, thirdPeriodGrade)
 				System.out.println(pick);
 				}
 				else if(pick == 2)
 				{
-			    System.out.println(pick);
+				//print list of arrays, at the bottom user picks which one to delete by number.
+			    System.out.println("Which Student would you like to delete?");
 				}
 			}
 
