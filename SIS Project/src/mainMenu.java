370805eb322797973aba84f058d832035efea709
Jack Paul <jackrpaul13@gmail.com>
2017-12-13 14:56:33 -0700

    fixed arrayList boi

--- SIS Project/src/mainMenu.java
+++ SIS Project/src/mainMenu.java
@@ -7,6 +7,7 @@ public class mainMenu
 		public static void main(String[] args) throws IOException
 			{
 			addStudents();
+			promptUser();
 			}
 		private static void addStudents() throws IOException
 			{
@@ -14,11 +15,15 @@ public class mainMenu
 				while(file.hasNextLine())
 					{
 						String line = file.nextLine();
-						System.out.println(line);
 						String [] catcher = line.split(" ");
 						int yes = 0;
 						roster.add(new Student(catcher[0], catcher[1], catcher[2], catcher[3], catcher[4], catcher[5], catcher[6], catcher[7], yes));
 					}
+			}
+		private static void promptUser()
+			{
+			//fixed it
+				
 			}
 
 	}
