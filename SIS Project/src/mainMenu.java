db820df5d876163c97501ae520b147075b00a5f8
Jack Paul <jackrpaul13@gmail.com>
2017-12-13 14:46:48 -0700

    made arrayList yeah BOY what up squad fam hey guys

--- SIS Project/src/mainMenu.java
+++ SIS Project/src/mainMenu.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class mainMenu
 	{
-		ArrayList <Student> roster = new ArrayList <Student>();
+		static ArrayList <Student> roster = new ArrayList <Student>();
 		public static void main(String[] args) throws IOException
 			{
 			addStudents();
@@ -13,10 +13,11 @@ public class mainMenu
 				Scanner file = new Scanner(new File("studentList.txt"));
 				while(file.hasNextLine())
 					{
-						
 						String line = file.nextLine();
 						System.out.println(line);
 						String [] catcher = line.split(" ");
+						int yes = 0;
+						roster.add(new Student(catcher[0], catcher[1], catcher[2], catcher[3], catcher[4], catcher[5], catcher[6], catcher[7], yes));
 					}
 			}
 
