f039fd1f6d5fdd57737daafd8be306979b7192e1
Jack Paul <jackrpaul13@gmail.com>
2017-12-14 14:07:28 -0700

    added GPA function, changed GPA to double rather than int

--- SIS Project/src/mainMenu.java
+++ SIS Project/src/mainMenu.java
@@ -16,8 +16,186 @@ public class mainMenu
 					{
 						String line = file.nextLine();
 						String [] catcher = line.split(" ");
-						int yes = 0;
-						roster.add(new Student(catcher[0], catcher[1], catcher[2], catcher[3], catcher[4], catcher[5], catcher[6], catcher[7], yes));
+						String fpg = catcher[3];
+						String spg = catcher[5];
+						String tpg = catcher[7];
+						double gpa = 0;
+						switch(fpg)
+							{
+								case "A+":
+										{
+										gpa += 4.0;
+										break;
+										}
+								case "A":
+										{
+										gpa += 4.0;
+										break;
+										}
+								case "A-":
+										{
+										gpa += 3.7;
+										break;
+										}
+								case "B+":
+										{
+										gpa += 3.3;
+										break;
+										}
+								case "B":
+										{
+										gpa += 3.0;
+										break;
+										}
+								case "B-":
+										{
+										gpa += 2.7;
+										break;
+										}
+								case "C+":
+										{
+										gpa += 2.3;
+										break;
+										}
+								case "C":
+										{
+										gpa += 2.0;
+										break;
+										}
+								case "C-":
+										{
+										gpa += 1.7;
+										break;
+										}
+								case "D+":
+										{
+										gpa += 1.3;
+										break;
+										}
+								case "D":
+										{
+										gpa += 1.0;
+										break;
+										}
+							}
+						switch(spg)
+						{
+							case "A+":
+									{
+									gpa += 4.0;
+									break;
+									}
+							case "A":
+									{
+									gpa += 4.0;
+									break;
+									}
+							case "A-":
+									{
+									gpa += 3.7;
+									break;
+									}
+							case "B+":
+									{
+									gpa += 3.3;
+									break;
+									}
+							case "B":
+									{
+									gpa += 3.0;
+									break;
+									}
+							case "B-":
+									{
+									gpa += 2.7;
+									break;
+									}
+							case "C+":
+									{
+									gpa += 2.3;
+									break;
+									}
+							case "C":
+									{
+									gpa += 2.0;
+									break;
+									}
+							case "C-":
+									{
+									gpa += 1.7;
+									break;
+									}
+							case "D+":
+									{
+									gpa += 1.3;
+									break;
+									}
+							case "D":
+									{
+									gpa += 1.0;
+									break;
+									}
+						}
+						switch(tpg)
+						{
+							case "A+":
+									{
+									gpa += 4.0;
+									break;
+									}
+							case "A":
+									{
+									gpa += 4.0;
+									break;
+									}
+							case "A-":
+									{
+									gpa += 3.7;
+									break;
+									}
+							case "B+":
+									{
+									gpa += 3.3;
+									break;
+									}
+							case "B":
+									{
+									gpa += 3.0;
+									break;
+									}
+							case "B-":
+									{
+									gpa += 2.7;
+									break;
+									}
+							case "C+":
+									{
+									gpa += 2.3;
+									break;
+									}
+							case "C":
+									{
+									gpa += 2.0;
+									break;
+									}
+							case "C-":
+									{
+									gpa += 1.7;
+									break;
+									}
+							case "D+":
+									{
+									gpa += 1.3;
+									break;
+									}
+							case "D":
+									{
+									gpa += 1.0;
+									break;
+									}
+						}
+						gpa /= 3;
+						roster.add(new Student(catcher[0], catcher[1], catcher[2], catcher[3], catcher[4], catcher[5], catcher[6], catcher[7], gpa));
 					}
 			}
 		private static void promptUser()
